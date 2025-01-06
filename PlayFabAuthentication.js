8e5fbe038a79a59ac3ce323b7600a1b06a0c4eb3
Ixy <heysilent359@gmail.com>
2025-01-06 17:57:07 +0000

    Update PlayFabAuthentication.js

--- PlayFabAuthentication.js
+++ PlayFabAuthentication.js
@@ -1,23 +1,11 @@
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const { username } = req.body;
-
-        // Mock logic for banned user
-        if (username === 'banned_user') {
-            return res.status(403).json({
-                BanMessage: 'Your account has been traced and you have been banned.',
-                BanExpirationTime: 'Indefinite',
-            });
-        }
-
-        // Success response for valid user
-        return res.status(200).json({
-            Message: 'Authentication successful',
-            SessionToken: 'abcdef123456',
-        });
-    }
-
-    // Unsupported HTTP method
-    res.setHeader('Allow', ['POST']);
-    res.status(405).json({ error: 'Method Not Allowed' });
+{
+    "MOTD": "<color=orange>WELCOME</color> <color=red>TO</color> <color=maroon>DRE1MER</color> <color=yellow>TAG</color>\n<color=grey>THANKS FOR PLAYING!</color>\n\n<color=yellow>IF YOU ENJOY THIS GAME, JOIN THE DISCORD TO SHOW SUPPORT AND RECOMMEND YOUR UPDATE IDEAS!</color>\n\n<color=purple>OWNER: </color><color=grey>BRODY</color>\n<color=magenta>CREDITS TO: DRE1MER, POLAR, REZ</color>\n<color=white>DISCORD.GG/DRE1MERTAG</color>",
+    "PrivacyPolicy_2023.05.16": "https://www.playfab.com/legal/privacy/",
+    "TOBAlreadyOwnCompTxt": "YOU OWN THE BUNDLE ALREADY! THANK YOU!",
+    "TOBAlreadyOwnPurchaseBtnTxt": "-",
+    "TOBDefCompTxt": "PLEASE SELECT A PACK TO TRY ON AND BUY",
+    "TOBDefPurchaseBtnDefTxt": "SELECT A PACK",
+    "TOBSafeCompTxt": "PURCHASE ITEMS IN YOUR CART AT THE CHECKOUT COUNTER",
+    "TOS_2023.05.16": "https://www.playfab.com/legal/tos/",
+    "TOTD": "[{\"PedestalID\":\"Pedestal1\",\"ItemName\":\"LBAFO.\",\"StartTimeUTC\":\"2024-03-08T23:00:00.000Z\",\"EndTimeUTC\":\"2024-03-09T23:00:00.000Z\"},{\"PedestalID\":\"Pedestal1\",\"ItemName\":\"LBAFF.\",\"StartTimeUTC\":\"2024-03-09T23:00:00.000Z\",\"EndTimeUTC\":\"2024-03-10T23:00:00.000Z\"}]"
 }
